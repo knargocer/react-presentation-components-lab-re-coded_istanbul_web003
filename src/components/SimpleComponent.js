0df352f7bc93bbaf37ba8548add16e009cf0da87
Knar Gocer <knargr@gmail.com>
2020-10-23 19:34:05 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,16 +1,24 @@
 // Code SimpleComponent Here
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 class SimpleComponent extends Component {
-  constructor(props) {
-    super(props);
- 
-    // Initial state here...
-    this.state = {
-      mood:"happy",
-    };
-  }
- 
-  render() {
+   
+    constructor() {
+        super()
+        this.state = {
+          mood: 'happy'
+        }
+      }
+
+      handleClick = () => {
+        this.setState({
+            mood: 'sad'
+        })
+    
+      }
+    
+
+    render() {
         return (
         <div onClick={this.handleClick}>
         {this.state.mood}
